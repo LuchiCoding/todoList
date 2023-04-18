035a8451a2d2c11cfbb5e7748a2683f7b0a8564c
LuchiCoding <122272592+LuchiCoding@users.noreply.github.com>
2023-04-18 15:27:13 +0200

    First commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.DB_HOST,{useNewUrlParser:true})
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_HOST,{useNewUrlParser:true});
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  
   } catch (error) {
     console.log(error);
     process.exit(1);
